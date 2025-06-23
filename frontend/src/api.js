2021802be9942510cdb9041026e390484d739d95
Alphanie Midelet <alphanie.midelet@ac-lyon.fr>
2025-06-23 16:16:54 +0200

    update endpoints.yaml

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
-const apiUrl = "/choreo-apis/django-react-notes-app/backend/v1"
+const apiUrl = "/choreo-apis/django-react-notes-app/backend/v1";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
@@ -11,12 +11,12 @@ api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem(ACCESS_TOKEN);
         if (token) {
-            config.headers.Authorization = `Bearer ${token}`
+            config.headers.Authorization = `Bearer ${token}`;
         }
         return config
     },
     (error) => {
-        return Promise.reject(error)
+        return Promise.reject(error);
     }
 )
 
